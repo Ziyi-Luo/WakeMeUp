69b0b26252c022b86ab16b635010052919f2108d
Yuechen-Zhao <zhaoyuechen@dyn-160-39-255-158.dyn.columbia.edu>
2016-05-06 18:01:53 -0400

    first commit from zyc

---
+++ get-data/multiframe_listener.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+import numpy as np
+import cv2
+import sys
+from pylibfreenect2 import Freenect2, SyncMultiFrameListener
+from pylibfreenect2 import FrameType, Registration, Frame
+
+try:
+    from pylibfreenect2 import OpenGLPacketPipeline
+    pipeline = OpenGLPacketPipeline()
+except:
+    from pylibfreenect2 import CpuPacketPipeline
+    pipeline = CpuPacketPipeline()
+
+fn = Freenect2()
+num_devices = fn.enumerateDevices()
+if num_devices == 0:
+    print("No device connected!")
+    sys.exit(1)
+
+serial = fn.getDeviceSerialNumber(0)
+device = fn.openDevice(serial, pipeline=pipeline)
+
+listener = SyncMultiFrameListener(
+    FrameType.Color | FrameType.Ir | FrameType.Depth)
+
+# Register listeners
+device.setColorFrameListener(listener)
+device.setIrAndDepthFrameListener(listener)
+
+device.start()
+
+
+while True:
+    frames = listener.waitForNewFrame()
+
+    color = frames["color"]
+    ir = frames["ir"]
+
+    # NOTE for visualization:
+    # cv2.imshow without OpenGL backend seems to be quite slow to draw all
+    # things below. Try commenting out some imshow if you don't have a fast
+    # visualization backend.
+    cv2.imshow("ir", ir.asarray() / 65535.)
+    cv2.imshow("color", cv2.resize(color.asarray(),
+                                   (int(1920 / 3), int(1080 / 3))))
+
+    listener.release(frames)
+
+    key = cv2.waitKey(delay=1)
+    if key == ord('q'):
+        break
+
+device.stop()
+device.close()
